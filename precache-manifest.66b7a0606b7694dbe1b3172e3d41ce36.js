f39bd2e112a3ffc03eab7e85abe44b3fdf581207
singhharshit24 <singhharshit2409@gmail.com>
2022-08-01 20:40:58 +0530

    Updates

---
+++ precache-manifest.66b7a0606b7694dbe1b3172e3d41ce36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d0b108faa174aa20c3e",
+    "url": "/Textify-app/static/css/main.72dc06d2.chunk.css"
+  },
+  {
+    "revision": "6d0b108faa174aa20c3e",
+    "url": "/Textify-app/static/js/main.98894f16.chunk.js"
+  },
+  {
+    "revision": "e2d705e298a43b92ad58",
+    "url": "/Textify-app/static/js/runtime~main.4d117e29.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/Textify-app/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/Textify-app/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "ec0ee06db0204eaa94203137da913d24",
+    "url": "/Textify-app/index.html"
+  }
+];
